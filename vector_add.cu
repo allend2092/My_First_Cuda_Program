c15e4368066d1d07f4d9926c38aa2d13dc81eb94
Daryl <42759316+allend2092@users.noreply.github.com>
2023-10-31 18:25:52 -0600

    Update vector_add.cu
    added comments

--- vector_add.cu
+++ vector_add.cu
@@ -1,48 +1,58 @@
-include <iostream>
+// Include necessary headers
+#include <iostream>
 #include <cuda_runtime.h>
 
+// Define constant for array size
 const int N = 512;
 
+// GPU kernel function to add two arrays
 __global__ void add(int *a, int *b, int *c) {
+    // Get the current block's index
     int tid = blockIdx.x;
+    
+    // Ensure we don't go out of bounds
     if (tid < N)
-        c[tid] = a[tid] + b[tid];
+        c[tid] = a[tid] + b[tid];  // Add corresponding elements of arrays a and b
 }
 
 int main() {
+    // Declare arrays of size N
     int a[N], b[N], c[N];
+    
+    // Declare pointers for device memory
     int *dev_a, *dev_b, *dev_c;
 
-    // Allocate device memory
+    // Allocate memory on the GPU for the arrays
     cudaMalloc((void**)&dev_a, N * sizeof(int));
     cudaMalloc((void**)&dev_b, N * sizeof(int));
     cudaMalloc((void**)&dev_c, N * sizeof(int));
 
-    // Initialize arrays
+    // Initialize arrays a and b with values
     for (int i = 0; i < N; i++) {
-        a[i] = -i;
-        b[i] = i * i;
+        a[i] = -i;          // Initialize array a with negative values
+        b[i] = i * i;       // Initialize array b with squares of indices
     }
 
-    // Copy arrays to device
+    // Copy initialized arrays a and b from host to device memory
     cudaMemcpy(dev_a, a, N * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(dev_b, b, N * sizeof(int), cudaMemcpyHostToDevice);
 
-    // Launch add() kernel on GPU with N blocks
+    // Launch the add kernel on the GPU with N blocks and 1 thread per block
     add<<<N,1>>>(dev_a, dev_b, dev_c);
 
-    // Copy array c back to host
+    // Copy the result from device to host memory
     cudaMemcpy(c, dev_c, N * sizeof(int), cudaMemcpyDeviceToHost);
 
-    // Display the result
+    // Display the results of the addition
     for (int i = 0; i < N; i++) {
         std::cout << a[i] << " + " << b[i] << " = " << c[i] << std::endl;
     }
 
-    // Cleanup
+    // Free the allocated device memory
     cudaFree(dev_a);
     cudaFree(dev_b);
     cudaFree(dev_c);
 
+    // Return 0 indicating successful execution
     return 0;
 }
